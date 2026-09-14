581bd5937c89510aa6d655ec7db94904e4e62873
jeongdaehun <daehun.jeong26@gmail.com>
2026-09-14 16:48:03 +0900

    '항목 삭제' 기능 코드 작성

--- text-based-household-account-book-cli/main.py
+++ text-based-household-account-book-cli/main.py
@@ -14,26 +14,29 @@
 - 항목은 파일에 저장하고 불러와야함(csv)
 '''
 import csv
-
+fieldnames = ['id', 'content', 'cost', 'total']
 
 while True:
     a = input("-"*50 + "\n원하시는 작업을 선택해주세요.\n1. 항목 추가\n2. 항목 삭제\n3. 잔액 조회\n4. 내역 조회\n5. 작업 종료\n번호 또는 작업을 입력해주세요.: ")
     if a == '1' or a == '항목 추가':
-        # 항목 추가할 때 필요한 매개변수들
+        # 항목 추가할 때 필요한 매개변수 초기화
         id = 0
-        content = input("항목의 내용을 입력해주세요.: ")
-        cost = input("항목의 비용을 입력해주세요.: ")
+        content = input("항목의 내용을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
+        if content == 'q': continue
+        cost = input("항목의 비용을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
         total = 0
 
         # cost를 정수 타입으로 잘 받기 위한 코드
-        b = True
-        while b:
-            try:
+        while True:
+            if cost == 'q':
+                break
+            elif not cost.lstrip('-').isdigit():
+                cost = input("잘못된 값이 입력되었습니다. 항목의 비용을 입력해주세요.: ")
+            else:
                 cost = int(cost)
-                b = False
-            except ValueError:
-                cost = input("항목의 비용을 입력해주세요.: ")
-
+                break
+        if cost == 'q': continue
+        
         # id와 total을 얻기 위한 코드
         with open("household-account.csv", "r", encoding="utf-8") as f:
             reader = csv.reader(f)
@@ -48,7 +51,55 @@ while True:
 
         print("항목이 작성 되었습니다.")
     elif a == '2' or a == '항목 삭제':
-        pass
+        # 항목 삭제 할 때 필요한 매개변수들
+        id = input("삭제할 항목의 id를 적어주세요. 취소를 원한다면 q를 입력해주세요.: ")
+
+        
+        # 'id를 양의 정수 타입으로 받기'+'가계부의 길이 보다 짧은 id를 받기' 위한 코드
+        with open('household-account.csv', 'r', encoding='utf-8') as f:
+                    reader = csv.reader(f)
+                    l = list(reader)[1:]
+        
+        while True:
+            if id == 'q':
+                break
+            elif not id.isdigit():
+                id = input("올바른 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
+            elif not int(id) < len(l):
+                id = input("값이 너무 큽니다. 올바른 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
+            else:
+                id = int(id)
+                break
+        if id == 'q': continue
+
+        # 삭제하려는게 정말 id가 맞는지 항목을 보여주면서 재검토
+        with open('household-account.csv', 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            yes_or_no = input(f'삭제하시려는 항목이 {list(reader)[id]}이 맞습니까? y 또는 n을 입력해주세요.: ')
+        while True:
+            if not yes_or_no == 'y' and not yes_or_no == 'n':
+                yes_or_no = input('올바른 값을 입력해주세요. y 또는 n을 입력해주세요.: ')
+            else:
+                break
+
+        # 답변이 n이면 다시 메뉴창으로 돌아가는 코드
+        if yes_or_no == 'n': continue
+        # 답변이 y면 id행을 지우는 코드
+        else:
+            with open('household-account.csv', 'r', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                reader_list = list(reader)[1:]
+            for x in reader_list:
+                if int(x[0]) == id:
+                    reader_list.remove(x)
+            if id != len(reader_list):
+                for x in reader_list[id:]:
+                    x[0] = int(x[0])-1
+            with open('household-account.csv', 'w', encoding='utf-8', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(fieldnames)
+                writer.writerows(reader_list)
+
     elif a == '3' or a == '잔액 조회':
         pass
     elif a == '4' or a == '내역 조회':
