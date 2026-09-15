c891cadf2bebff1f3b64517e9dc71bc9d2a0853b
jeongdaehun <daehun.jeong26@gmail.com>
2026-09-15 10:20:45 +0900

    인터페이스 약간 꾸밈

--- text-based-household-account-book-cli/main.py
+++ text-based-household-account-book-cli/main.py
@@ -3,59 +3,62 @@
 ## 기획
 ### 기능
 - 항목 추가/삭제 : 입력값을 토대로 항목 추가/삭제하기
-└ 추가 : 내용, 가격, [인덱스는 자동으로 추가됨], [현재 잔액은 자동으로 계산됨]
-└ 삭제 : 내용 또는 인덱스를 받아서 삭제하기.
+└ 추가 : 내용, 가격 [인덱스는 자동으로 추가됨], [현재 잔액은 자동으로 계산됨]
+└ 삭제 : 인덱스를 받아서 삭제하기.
 - 잔액 자동 계산 : 항목에 따라서 전체 잔액 바로바로 계산하기
 - 잔액 조회 : 그냥 현재 잔액 보여주기
-- 가계부 조회 : 원하는 길이 만큼 조회하고, 입력값이 길이를 넘으면 모든 걸 보여주기.
-최대 길이는 30줄로 제한
+- 내역 조회 : 원하는 길이 만큼 최근 내역부터 조회하고, 입력값이 길이를 넘으면 모든 걸 보여주기.
 
 ### 조건
 - 항목은 파일에 저장하고 불러와야함(csv)
 '''
 import csv
-fieldnames = ['id', 'content', 'cost', 'total']
+fieldnames = ['id', 'content', 'cost', 'total'] # csv파일 열 이름
 
+# 메뉴창
 while True:
     a = input("-"*50 + "\n원하시는 작업을 선택해주세요.\n1. 항목 추가\n2. 항목 삭제\n3. 잔액 조회\n4. 내역 조회\n5. 작업 종료\n번호 또는 작업을 입력해주세요.: ")
     if a == '1' or a == '항목 추가':
-        # 항목 추가할 때 필요한 매개변수 초기화
+        # 혹시 몰라서 넣은 매개변수 초기화
         id = 0
-        content = input("항목의 내용을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
-        if content == 'q': continue
-        cost = input("항목의 비용을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
         total = 0
+
+        # content와 cost를 입력받는 코드
+        content = input("-"*50 + "\n항목의 내용을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
+        if content == 'q': continue
+        cost = input("-"*50 + "\n항목의 비용을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
+        
 
         # cost를 정수 타입으로 잘 받기 위한 코드
         while True:
             if cost == 'q':
                 break
             elif not cost.lstrip('-').isdigit():
-                cost = input("잘못된 값이 입력되었습니다. 항목의 비용을 입력해주세요.: ")
+                cost = input("-"*50 + "\n잘못된 값이 입력되었습니다. 항목의 비용을 입력해주세요.: ")
             else:
                 cost = int(cost)
                 break
         if cost == 'q': continue
         
-        # id와 total을 얻기 위한 코드
+        # id와 total을 csv에서 얻는 코드
         with open("household-account.csv", "r", encoding="utf-8") as f:
             reader = csv.reader(f)
             l = list(reader)[-1]
             id = int(l[0]) # 단순히 마지막 줄을 보고 싶을 뿐인데 모든 값을 리스트로 만드는게 맞나?
             total = int(l[3])
 
-        # 모든 매개변수들을 csv의 마지막 줄에 추가하는 코드
+        # 모든 매개변수들을 가계부에 추가하는 코드
         with open("household-account.csv", "a", newline='', encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerow([id+1, content, cost, total+cost])
 
-        print("항목이 작성 되었습니다.")
+        print("-"*50 + "\n항목이 작성 되었습니다.")
     elif a == '2' or a == '항목 삭제':
-        # 항목 삭제 할 때 필요한 매개변수들
-        id = input("삭제할 항목의 id를 적어주세요. 취소를 원한다면 q를 입력해주세요.: ")
+        # 삭제할 id를 받는 코드
+        id = input("-"*50 + "\n삭제할 항목의 id를 적어주세요. 취소를 원한다면 q를 입력해주세요.: ")
 
         
-        # 'id를 양의 정수 타입으로 받기'+'가계부의 길이 보다 짧은 id를 받기' 위한 코드
+        # 'id를 양의 정수 타입으로 받기'+'가계부의 길이 보다 긴 id를 받았을 때 처리하기' 위한 코드
         with open('household-account.csv', 'r', encoding='utf-8') as f:
                     reader = csv.reader(f)
                     l = list(reader)[1:]
@@ -64,9 +67,9 @@ while True:
             if id == 'q':
                 break
             elif not id.isdigit():
-                id = input("올바른 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
+                id = input("-"*50 + "\n올바른 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
             elif not int(id) < len(l):
-                id = input("값이 너무 큽니다. 올바른 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
+                id = input("-"*50 + "\n값이 너무 큽니다. 올바른 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
             else:
                 id = int(id)
                 break
@@ -78,7 +81,7 @@ while True:
             yes_or_no = input(f'삭제하시려는 항목이 {list(reader)[id]}이 맞습니까? y 또는 n을 입력해주세요.: ')
         while True:
             if not yes_or_no == 'y' and not yes_or_no == 'n':
-                yes_or_no = input('올바른 값을 입력해주세요. y 또는 n을 입력해주세요.: ')
+                yes_or_no = input("-"*50 + '\n올바른 값을 입력해주세요. y 또는 n을 입력해주세요.: ')
             else:
                 break
 
@@ -134,7 +137,8 @@ while True:
             elif not line_count.isdigit():
                 line_count = input("올바른 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
             elif not int(line_count) < len(l):
-                line_count = input("값이 너무 큽니다. 올바른 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
+                print('입력 값이 내역의 길이보다 커서 내역 전체를 출력합니다.')
+                line_count = len(l)
             elif line_count == '0':
                 line_count = input("0보다 큰 값을 입력해주세요. 취소를 원한다면 q를 입력해주세요.: ")
             else:
